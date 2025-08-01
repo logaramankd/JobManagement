b2c6dd6a759ee5d7205b7e568201428d0bb67178
Logaraman <logaraman2403@gmail.com>
2025-08-01 17:53:38 +0530

    railway connection

--- job_backend/src/seeds/seed.js
+++ job_backend/src/seeds/seed.js
@@ -8,16 +8,17 @@ exports.seed = async function (knex) {
   await knex.raw('ALTER TABLE jobs AUTO_INCREMENT = 1');
 
   await knex('jobs').insert([
-    {
-      job_title: 'Full Stack Developer',
-      company_name: 'Amazon',
-      location: 'Bengaluru',
+     {
+      job_title: 'UI/UX Designer',
+      company_name: 'swiggy',
+      location: 'Chennai',
       job_type: 'Full-time',
-      salary_min: 0,
-      salary_max: 1200000,
-      application_deadline: '2025-08-10',
-      job_description: 'Build frontend and backend for scalable applications.'
-    }
+      salary_min: 30000,
+      salary_max: 50000,
+      application_deadline: '2025-08-01',
+      job_description:
+        'A user friendly interface let you browse stunning photos and videos\nFilter destination based on interest and travel style, and create personalized',
+    },
   ]);
 };
 
